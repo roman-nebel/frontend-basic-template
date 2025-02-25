4cb32ad77d91b9a890213bc632a1294b4270f4e8
Roman Nebel <im.roman.nebel@gmail.com>
2025-02-25 11:29:55 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={'/'} element={<></>} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/api/controller.ts
@@ -0,0 +1,12 @@
+export async function get(endpoint: string, options: Object) {
+  try {
+    /*
+    const responce = await fetch("baseURI + endpoint", {
+      method: "GET"
+    })
+    */
+    return true
+  } catch {
+    return false
+  }
+}

1402f23cd80f90934d50b221957a480ac77a301a
Roman Nebel <r@nebel.im>
2026-02-03 11:29:28 +0100

    Add initial HTML template and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({ plugins: [react()], server: { open: true } });
+export default defineConfig({
+	root: "public",
+	publicDir: false,
+	plugins: [react()],
+	server: { open: true },
+	build: { outDir: "../dist", emptyOutDir: true },
+});
